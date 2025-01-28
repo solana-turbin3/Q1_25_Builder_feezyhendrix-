7488589612168076ddb6f6dcd94623c0f1a4b304
AbdulHafeez AbdulRaheem <switchdevelop1@gmail.com>
2025-01-28 11:34:29 +0100

    feat: added update

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,16 +1,34 @@
 use anchor_lang::prelude::*;
 
 declare_id!("DyPwvtAcL7utnYfmVSVRXCyiirE8CTWLvu3ogidrHBLY");
+pub mod instructions;
+pub mod state;
+use crate::instructions::*;
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make(
+        ctx: Context<Make>,
+        seed: u64,
+        receive_amount: u64,
+        deposit_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .init_escrow_state(seed, receive_amount, ctx.bumps)?;
+        ctx.accounts.deposit(deposit_amount)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.withdraw()?;
+        ctx.accounts.close()?;
+        Ok(())
+    }
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.withdraw()?;
+        ctx.accounts.close()?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
